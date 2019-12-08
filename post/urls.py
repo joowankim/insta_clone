003e3efcbf03d155437cd8e7136c697a9c1081a9
kjw217 <rlawndhks217@gmail.com>
2019-12-08 13:02:39 +0900

    새글, 수정, 삭제   urls, view   forms   html   js   edit   delete

--- post/urls.py
+++ post/urls.py
@@ -5,4 +5,7 @@ app_name = 'post'
 
 urlpatterns = [
     path('', post_list, name='post_list'),  # view.py에 선언된 post_list함수를 실행
+    path('new', post_new, name='post_new'),
+    path('edit/<int:pk>', post_edit, name='post_edit'),
+    path('delete/<int:pk>', post_delete, naem='post_delete'),
 ]
