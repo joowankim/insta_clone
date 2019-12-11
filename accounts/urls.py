b6f07539930cd46797d29be3e106013b968a6ecb
kjw217 <rlawndhks217@gmail.com>
2019-12-11 18:30:10 +0900

    팔로우   models, admin   html, ajax   urls, views

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', login_check, name='login'),
     path('logout/', logout, name='logout'),
+
+    path('follow/', follow, name='follow'),
 ]
