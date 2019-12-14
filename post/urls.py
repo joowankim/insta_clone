36f0442593667013071db77e03a1fd558c7f806d
kjw217 <rlawndhks217@gmail.com>
2019-12-14 19:53:00 +0900

    태그 검색   model, admin, extra   view, url

--- post/urls.py
+++ post/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
     path('comment/new', comment_new, name='comment_new'),
     path('comment/delete', comment_delete, name='comment_delete'),
 
+    path('explore/tags/<tag>/', post_list, name='post_search'),
 ]
